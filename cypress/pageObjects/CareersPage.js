d50a3f35ea707535b54018b64fa54666c9c9e10f
Carlos-44 <cjozoria@gmail.com>
2024-08-26 13:07:28 -0400

    Update CareersPage.js

--- cypress/pageObjects/CareersPage.js
+++ cypress/pageObjects/CareersPage.js
@@ -1,6 +1,14 @@
 class CareersPage {
     navigateToCareers() {
-      cy.contains('Careers').scrollIntoView().should('be.visible').click();
+      cy.contains('Careers').scrollIntoView().click();
+      cy.log('Navigated to Careers page');
+    }
+  
+    verifyFindJobsText() {
+      cy.origin('https://www.amazon.jobs', () => {
+        cy.contains('Find jobs').should('be.visible');
+        cy.log('Verified Find Jobs text is visible');
+      });
     }
   }
   
