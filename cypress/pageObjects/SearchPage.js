57ce519234bbfa33e0e54a1fabbf397977176106
Carlos-44 <cjozoria@gmail.com>
2024-08-26 13:08:46 -0400

    Update SearchPage.js

--- cypress/pageObjects/SearchPage.js
+++ cypress/pageObjects/SearchPage.js
@@ -1,10 +1,22 @@
 class SearchPage {
-    verifySearchResults(item) {
-      cy.get('.s-main-slot').should('contain', item);
+    verifySearchResults(text) {
+      cy.get('.s-main-slot').should('contain.text', text);
+      cy.log(`Verified search results contain: ${text}`);
     }
   
     selectFirstResult() {
       cy.get('.s-main-slot .s-result-item').first().click();
+      cy.log('Selected the first search result');
+    }
+  
+    filterResultsByBrand(brand) {
+      cy.contains(brand).click();
+      cy.log(`Filtered results by brand: ${brand}`);
+    }
+  
+    sortBy(option) {
+      cy.get('#s-result-sort-select').select(option);
+      cy.log(`Sorted results by option: ${option}`);
     }
   }
   
