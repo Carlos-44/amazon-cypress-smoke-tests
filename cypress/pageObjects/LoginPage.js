e5ab786c686e41d386d0fc763c73f942ab6fdb06
Carlos-44 <cjozoria@gmail.com>
2024-08-26 13:08:30 -0400

    Update LoginPage.js

--- cypress/pageObjects/LoginPage.js
+++ cypress/pageObjects/LoginPage.js
@@ -1,24 +1,23 @@
 class LoginPage {
-    enterEmail(email) {
-      cy.get('[name="email"]').type(email);
-    }
-  
-    submitEmail() {
-      cy.get('[type="submit"]').click();
-    }
-  
-    enterPassword(password) {
-      cy.get('[name="password"]').type(password);
-    }
-  
-    submitLogin() {
-      cy.get('[id="signInSubmit"]').click();
-    }
-  
-    verifyCaptcha() {
-      cy.contains('Solve this puzzle to protect your account').should('be.visible');
-    }
+  enterEmail(email) {
+    cy.get('[name="email"]').type(email);
   }
-  
-  export default LoginPage;
-  
+
+  submitEmail() {
+    cy.get('[type="submit"]').click();
+  }
+
+  enterPassword(password) {
+    cy.get('[name="password"]').type(password);
+  }
+
+  submitLogin() {
+    cy.get('[id="signInSubmit"]').click();
+  }
+
+  verifyCaptcha() {
+    cy.contains('Solve this puzzle to protect your account').should('be.visible');
+  }
+}
+
+export default LoginPage;
